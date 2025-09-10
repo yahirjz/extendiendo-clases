d7a1bbe42e79b82639ba3f61e7ca0544c2094b9b
Jaime Jair Rodriguez Juarez <yair@Jaimes-MacBook-Air.local>
2025-09-10 15:07:33 -0600

    Implemento ListaDeProductos con carga desde product.json

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,68 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+constructor(name:string){
+super(name)
+
+// IMPORTANTE
+const contenidoDelArchivo = fs.readFileSync(__dirname + "/products.json").toString()
+const productosDelArchivo = JSON.parse(contenidoDelArchivo);
+productosDelArchivo.forEach(prod => {
+  this.addProduct(prod)
+})
+}
+////
+addProduct(product: Product):void{
+
+const existe = this.cosas.some(prod => prod.id === product.id)
+//usasmos .some() para ver si existe el producto por que nos devuelve True o false 
+//BUscamos si no existe el producto.
+if(existe){
+  console.log(`El producto con el id ${product.id} ya existe`);
+  return;
+}
+//Si no existe el producto con el "id" agregamos el prodcuto
+  this.cosas.push(product); 
+  return;
+}
+
+getProduct(id: number): Product{
+//buscamios el producto
+const productEncontrado = this.cosas.find(prod => prod.id === id);
+
+return productEncontrado;
+}
+
+removeProduct(id: number): void{
+//Buscamos su exist el producto con el id
+const existe = this.cosas.some( prod => prod.id === id)
+
+//Si no existe el producto con el id no mandamos un msj 
+if(!existe){
+  console.log(`El producto con id ${id} no existe`);
+  return;
+}
+//sino lo eliminamos 
+this.cosas = this.cosas.filter(prod => prod.id !== id);
+}
+
+getSortedByPrice(order: string): Product[]{
+//Clonamos el array para no modificar el original
+const sortedCosas = [...this.cosas];
+
+//ordenamos el array segun su valor
+sortedCosas.sort((a,b) =>{
+  if(order == "asc"){
+    return a.price - b.price //Orden ascendente
+  }else if( order == "desc"){
+    return b.price -a.price;
+  }
+  return 0; // si no es "asc" ni "desc", no se ordena 
+})
+return sortedCosas;
+}
+
+}
 
 export { ListaDeProductos, Product };
